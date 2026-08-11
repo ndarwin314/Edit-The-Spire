7f13ac8f7ce7b42085b8029976ecd0a800a20be4
mathy <ndarwin@comcast.net>
2026-08-11 12:33:48 -0700

    Initial json parsing and commands exposing values

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,79 @@
+mod save;
+
+use std::fs;
+use std::path::Path;
+use std::sync::Mutex;
+
+use tauri::{Builder, Manager, State};
+use serde_json::{Result, Value};
+use crate::save::{AppState, Relic, SaveFile, Potion, Card};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn open_file(file_name: String, state: State<'_, Mutex<AppState>>) {
+    let path = Path::new(&file_name);
+    let contents = fs::read_to_string(path)
+        .expect("Should have been able to read the file");
+    let save: SaveFile = serde_json::from_str(&contents).expect("Should have been able to deserialize");
+    let index = 0;
+    let mut state = state.lock().unwrap();
+    state.save = save;
+    state.index = index;
+}
+
+#[tauri::command]
+fn get_health(state: State<'_, Mutex<AppState>>) -> (i32, i32){
+    let state = state.lock().unwrap();
+    let player = &state.save.players[state.index];
+    (player.current_hp, player.max_hp)
+}
+
+#[tauri::command]
+fn get_gold(state: State<'_, Mutex<AppState>>) -> i32 {
+    let state = state.lock().unwrap();
+    let player = &state.save.players[state.index];
+    player.gold
+}
+
+#[tauri::command]
+fn get_artifacts(state: State<'_, Mutex<AppState>>) -> Vec<Relic> {
+    let state = state.lock().unwrap();
+    let player = &state.save.players[state.index];
+    player.relics.clone()
+}
+
+#[tauri::command]
+fn get_potions(state: State<'_, Mutex<AppState>>) -> (i32, Vec<Potion>) {
+    let state = state.lock().unwrap();
+    let player = &state.save.players[state.index];
+    (player.max_potion_slot_count, player.potions.clone())
+}
+
+#[tauri::command]
+fn get_deck(state: State<'_, Mutex<AppState>>) -> Vec<Card> {
+    let state = state.lock().unwrap();
+    let player = &state.save.players[state.index];
+    player.deck.clone()
+}
+
+
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    Builder::default()
+        .setup(|app| {
+        app.manage(Mutex::new(AppState::default()));
+        Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![open_file, get_health, get_gold, get_artifacts, get_potions, get_deck])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/save.rs
@@ -0,0 +1,123 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Player {
+    pub base_orb_slot_count: i32,
+    pub character_id: String,
+    pub current_hp: i32,
+    pub deck: Vec<Card>,
+    pub extra_fields: Value,
+    pub gold: i32,
+    pub max_energy: i32,
+    pub max_hp: i32,
+    pub max_potion_slot_count: i32,
+    pub net_id: i32,
+    pub odds: Odds,
+    pub relic_grab_bag: RelicGrabBag,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub relics: Vec<Relic>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub potions: Vec<Potion>,
+    pub rng: PlayerRng,
+    pub unlock_state: UnlockState,
+
+    #[serde(flatten)]
+    pub other: serde_json::Map<String, Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Card {
+    pub floor_added_to_deck: i32,
+    pub id: String,
+    pub current_upgrade_level: u8,
+    pub enchantment: Enchantment,
+    pub props: Value,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Enchantment {
+    pub id: String,
+    pub amount: i32,
+    pub props: Value,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CardModifiers {
+    pub base_lib_card_modifiers: Vec<Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Potion {
+    pub id: String,
+    pub slot_index: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Relic {
+    pub floor_added_to_deck: i32,
+    pub id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Odds {
+    pub card_rarity_odds_value: f64,
+    pub potion_reward_odds_value: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct RelicGrabBag {
+    pub relic_id_lists: RelicIdLists,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct RelicIdLists {
+    pub uncommon: Vec<String>,
+    pub common: Vec<String>,
+    pub rare: Vec<String>,
+    pub shop: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct PlayerRng {
+    pub counters: RngCounters,
+    pub seed: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct RngCounters {
+    pub rewards: i32,
+    pub shops: i32,
+    pub transformations: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct UnlockState {
+    pub encounters_seen: Vec<String>,
+    pub number_of_runs: i32,
+    pub unlocked_epochs: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SaveFile {
+    pub players: Vec<Player>,
+
+    #[serde(flatten)]
+    pub other: serde_json::Map<String, Value>,
+}
+
+pub struct AppState {
+    pub save: SaveFile,
+    pub index: usize
+}
+
+impl Default for AppState {
+    fn default() -> AppState {
+        AppState {
+            index: 0,
+            save: SaveFile{ players: vec![], other: serde_json::Map::new() },
+        }
+    }
+}
+
+
